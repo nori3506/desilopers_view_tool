5151a54487a4ca838acc452ebe318551eabb1780
Nori <loveandfight2186@yahoo.co.jp>
2020-05-07 00:01:14 +0900

    initial commit

---
+++ lib/desilopers_view_tool.rb
@@ -0,0 +1,7 @@
+require "desilopers_view_tool/version"
+require "desilopers_view_tool/renderer"
+
+module DesilopersViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/desilopers_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DesilopersViewTool
+  VERSION = "0.1.0"
+end
